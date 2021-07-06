341e6383fa5d756b89930da5840f91098653a72e
hrloca <hr.loca@gmail.com>
2021-07-06 19:24:41 +0900

    update

--- src/domain/entities/MichiekiUser/MichiekiUserID.ts
+++ src/domain/entities/MichiekiUser/MichiekiUserID.ts
@@ -1,15 +1,18 @@
 import { ID } from '../../core'
 
 export class MichiekiUserID extends ID {
-  private readonly maxCount: number = 24
+  readonly maxCount: number = 24
+  readonly minCount: number = 4
   constructor(readonly source: string) {
     super()
     this.verifyStringCount()
   }
 
   verifyStringCount() {
-    if (this.source.length > this.maxCount) {
-      throw new Error(`idの長さが${this.maxCount}を超えています。`)
-    }
+    if (this.source.length > this.maxCount)
+      throw new Error(`idの長さが最大値を超えています。`)
+
+    if (this.source.length < this.minCount)
+      throw new Error(`idの長さが最小値を下回っています。`)
   }
 }
