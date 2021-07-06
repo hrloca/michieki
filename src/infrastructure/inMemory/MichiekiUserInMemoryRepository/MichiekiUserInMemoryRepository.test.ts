ec32bf49ff5faf4ca35db7e0a0604ee65c4b9040
hrloca <hr.loca@gmail.com>
2021-07-06 19:35:10 +0900

    update

--- src/infrastructure/inMemory/MichiekiUserInMemoryRepository/MichiekiUserInMemoryRepository.test.ts
+++ src/infrastructure/inMemory/MichiekiUserInMemoryRepository/MichiekiUserInMemoryRepository.test.ts
@@ -14,12 +14,12 @@ describe('MichiekiUserInMemoryRepository test.', () => {
   const ds = new MichiekiUserInMemoryDS(new Map<string, MichiekiUser>())
 
   const a = new MichiekiUser(
-    new MichiekiUserID('a'),
+    new MichiekiUserID('asan'),
     new MichiekiUserDisplayName('a:san')
   )
 
   const b = new MichiekiUser(
-    new MichiekiUserID('b'),
+    new MichiekiUserID('bsan'),
     new MichiekiUserDisplayName('b:san')
   )
 
@@ -29,7 +29,7 @@ describe('MichiekiUserInMemoryRepository test.', () => {
   const repos = new MichiekiUserInMemoryRepository(ds)
 
   it('IDで対象のMichiekiUserが取得できる', async () => {
-    const result = await repos.findById(new MichiekiUserID('a'))
+    const result = await repos.findById(new MichiekiUserID('asan'))
 
     if (!result) throw new Error('何かがおかしいです')
 
@@ -39,20 +39,20 @@ describe('MichiekiUserInMemoryRepository test.', () => {
 
   it('MichiekiUserを登録できる', async () => {
     const c = new MichiekiUser(
-      new MichiekiUserID('c'),
+      new MichiekiUserID('csan'),
       new MichiekiUserDisplayName('c:san')
     )
 
     const result = await repos.store(c)
     if (!result) throw new Error('何かがおかしいです')
 
-    expect(c.id.equals(new MichiekiUserID('c'))).toBe(true)
+    expect(c.id.equals(new MichiekiUserID('csan'))).toBe(true)
     expect(c.displayName.toString()).toBe('c:san')
   })
 
   it('MichiekiUserを更新できる', async () => {
     const d = new MichiekiUser(
-      new MichiekiUserID('d'),
+      new MichiekiUserID('dsan'),
       new MichiekiUserDisplayName('d:san')
     )
 
@@ -60,14 +60,14 @@ describe('MichiekiUserInMemoryRepository test.', () => {
     if (!result) throw new Error('何かがおかしいです')
 
     const new_d = new MichiekiUser(
-      new MichiekiUserID('d'),
+      new MichiekiUserID('dsan'),
       new MichiekiUserDisplayName('d:sama')
     )
 
     const result2 = await repos.store(new_d)
     if (!result2) throw new Error('何かがおかしいです')
 
-    const result3 = await repos.findById(new MichiekiUserID('d'))
+    const result3 = await repos.findById(new MichiekiUserID('dsan'))
     if (!result3) throw new Error('何かがおかしいです')
 
     expect(result3.displayName.toString()).toBe('d:sama')
